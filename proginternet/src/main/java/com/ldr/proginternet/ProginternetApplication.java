334f24630ccee94de0f39e9cecaf767d0d6d7d55
𝙶𝚞𝚒𝚕𝚑𝚎𝚛𝚖𝚎 𝚂𝚊𝚗𝚝𝚘 𝙲𝚘𝚜𝚝𝚊 <124518355+guizitos@users.noreply.github.com>
2024-11-02 01:19:57 -0300

    Add files via upload

---
+++ proginternet/src/main/java/com/ldr/proginternet/ProginternetApplication.java
@@ -0,0 +1,13 @@
+package com.ldr.proginternet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProginternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProginternetApplication.class, args);
+	}
+
+}
